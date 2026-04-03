ed0724425f6d02a46c32c3f86e05f48b2f15de6c
Daniel T. Fijalka <daniel.fijalka@hobbylobby.com>
2026-04-03 13:59:55 -0500

    wire up main.js with PrimeVue, Pinia, and router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div>
+    <RouterView />
+  </div>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).useRouter.mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.mount('#app')
